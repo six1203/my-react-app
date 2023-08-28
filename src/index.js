bd1b5771a5917c66fd1d92c55b95897ae316f317
dylan <u1s10605@126.com>
2023-08-28 21:19:23 +0800

    feat: learn React State(状态)

--- src/index.js
+++ src/index.js
@@ -72,39 +72,103 @@ import ReactDOM from 'react-dom';
 // }
 // setInterval(tick, 1);
 
-var myStyle = {color: "red", textAlign: "center"}
+// var myStyle = {color: "red", textAlign: "center"}
+//
+// class Name extends React.Component {
+//     render() {
+//         return <h1 style={myStyle}>网站名称： {this.props.name}</h1>
+//     }
+// }
+//
+// class Url extends  React.Component {
+//     render() {
+//         return <h1>网站地址：{this.props.url}</h1>
+//     }
+// }
+//
+// class Nickname extends React.Component {
+//     render() {
+//         return <h1>网站用户：{this.props.nickname}</h1>
+//     }
+// }
+//
+//
+// function App(props) {
+//     return (
+//         <div>
+//             <Name name={props.name}/>
+//             <Url url={props.name}/>
+//             <Nickname nickname={props.nickname}/>
+//         </div>
+//     )
+// }
+//
+//
+// ReactDOM.render(
+//     <App name={"菜鸟教程"} url={"http://www.runoob.com"} nickname={"Runoob"} />,
+//     document.getElementById('example')
+// )
 
-class Name extends React.Component {
+// const arr = [
+//     <h1>菜鸟教程</h1>,
+//     <h2>学的不仅是技术，更是梦想！</h2>
+// ];
+//
+// ReactDOM.render(
+//     <div>{arr}</div>,
+//     document.getElementById('example')
+// )
+
+class  Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()}
+    }
+
+    componentDidMount() {
+        // this.timerID = setInterval(
+        //     () => this.tick(), 1000
+        // );
+
+        let _this = this;
+        this.timerID = setInterval(function(){
+            _this.tick();
+        }, 1000);
+    }
+
+
+    componentWillUnmount() {
+        clearInterval(this.timerID)
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        })
+    }
+
     render() {
-        return <h1 style={myStyle}>网站名称： {this.props.name}</h1>
+        return (
+            <div>
+                <h1>hello, world</h1>
+                <h2>现在是{this.state.date.toLocaleTimeString()}.</h2>
+            </div>
+        )
     }
 }
 
-class Url extends  React.Component {
-    render() {
-        return <h1>网站地址：{this.props.url}</h1>
-    }
-}
 
-class Nickname extends React.Component {
-    render() {
-        return <h1>网站用户：{this.props.nickname}</h1>
-    }
-}
-
-
-function App(props) {
+function App() {
     return (
         <div>
-            <Name name={props.name}/>
-            <Url url={props.name}/>
-            <Nickname nickname={props.nickname}/>
+            <Clock />
+            <Clock />
+            <Clock />
         </div>
     )
 }
 
-
 ReactDOM.render(
-    <App name={"菜鸟教程"} url={"http://www.runoob.com"} nickname={"Runoob"} />,
+    <App />,
     document.getElementById('example')
 )
